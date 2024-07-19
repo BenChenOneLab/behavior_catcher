18769dc39c55ec98f2872e03de8aa966885b2935
ben.chen <ben.chen@onelab.tw>
2024-07-19 15:31:08 +0800

    CDS-352 feat: Add script for downloading user login data
    The new script `user_login_download.py` is added to the repository. It uses the Google Cloud BigQuery library to download user login data for a specified date range. The downloaded data is saved as CSV files.

---
+++ user_login_download.py
@@ -0,0 +1,41 @@
+from google.cloud import bigquery
+import os
+from datetime import datetime, timedelta
+from multiprocessing import Pool
+from functools import partial
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(
+    os.getenv("HOME"), "work/behavior-catcher-9adb879ec19d.json"
+)
+
+client = bigquery.Client()
+
+
+def download_data(base_date, day_offset):
+    current_date = base_date + timedelta(days=day_offset)
+    next_date = current_date + timedelta(days=1)
+
+    QUERY = f"""
+        SELECT siteid, sitename, IBCCustId, Currency, doing, logdate, IP, Status, ActionType, OS, OS_version, browser, browser_version, Platform, Action, LoginUrl
+        FROM `nf-bifrost.KeyIndex.user_login` 
+        WHERE logdate BETWEEN DATETIME("{current_date.strftime('%Y-%m-%d')}") 
+        AND DATETIME("{next_date.strftime('%Y-%m-%d')}") 
+    """
+    print(f"start to download data for {current_date.strftime('%Y-%m-%d')}")
+    query_job = client.query(QUERY)  # API request
+    df = query_job.to_dataframe()
+    print(f"len(df) for {current_date.strftime('%Y-%m-%d')}: {len(df)}")
+    filename = os.path.join(
+        os.getenv("HOME"), f'work/behavior/login_{current_date.strftime("%Y%m%d")}.csv'
+    )
+    df.to_csv(filename, index=False)
+
+    print(f'Data for {current_date.strftime("%Y-%m-%d")} saved to {filename}')
+
+
+if __name__ == "__main__":
+    print("start")
+    base_date = datetime(2024, 6, 1)
+    download_data_with_base = partial(download_data, base_date)
+    with Pool(processes=30) as pool:
+        pool.map(download_data_with_base, range(30))
